5527964d040eba1b9e1b9121bceff0509bab158c
hussainweb <hussainweb@gmail.com>
2025-09-02 18:31:24 -0400

    chore: remove asdf installation
    Install asdf plugins only if asdf is somehow installed on the machine (through brew for example)

--- run_once_after_10_asdf.sh
+++ run_once_after_10_asdf.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env fish
 
+if ! command -v asdf &> /dev/null
+  echo "asdf not installed. Skipping installing plugins..."
+  exit
+end
+
 asdf plugin add php || true
 asdf plugin add python || true
 asdf plugin add golang || true
--- run_once_before_10_install_asdf.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf 2> /dev/null || true
-git -C ~/.asdf checkout "$(git -C ~/.asdf describe --abbrev=0 --tags)" 2> /dev/null || true
-
-cp ~/.asdf/completions/asdf.fish ~/.config/fish/completions
